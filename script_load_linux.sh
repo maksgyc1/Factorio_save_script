6ae73760236c677288c8367a688b57f1d74517e0
Щипицин М.В. <maksgyc1@gmail.com>
2024-10-26 18:58:28 +0300

    save

--- script_load_linux.sh
+++ script_load_linux.sh
@@ -1,3 +1,3 @@
 git pull
-cp aboba_base.zip /home/maks/Factorio_save_script/
-/home/maks/Загрузки/Factorio_Linux/factorio-space-age_linux_2.0.8/factorio/bin/x64/factorio
+cp aboba_base.zip /home/maks/Загрузки/Factorio_Linux/factorio-space-age_linux_2.0.11/factorio/saves
+/home/maks/Загрузки/Factorio_Linux/factorio-space-age_linux_2.0.11/factorio/bin/x64/factorio
