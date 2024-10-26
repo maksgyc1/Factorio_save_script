09005c25d8c5357506f20d6a04d2d84b63f5277b
Max <maksgyc1@gmail.com>
2024-10-26 17:36:47 +0300

    add_win

--- script_load_linux.sh
+++ script_load_linux.sh
@@ -1,3 +1,3 @@
 git pull
 cp aboba_base.zip /home/maks/Factorio_save_script/
-D:/Games/FactorioSpaceAgev2.0.11a/Launcher.bat
+/home/maks/Загрузки/Factorio_Linux/factorio-space-age_linux_2.0.8/factorio/bin/x64/factorio
--- script_load_win.sh
+++ script_load_win.sh
@@ -1,2 +1,4 @@
 git pull
 cp aboba_base.zip cp C:/Users/maksg/AppData/Roaming/Factorio/saves/
+ls
+D:/Games/FactorioSpaceAgev2.0.11a/bin/x64/factorio.exe
