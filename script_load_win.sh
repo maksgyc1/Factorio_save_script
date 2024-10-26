8122b743de31831b4a57478ad1e827f5c6c9315e
Max <maksgyc1@gmail.com>
2024-10-26 17:27:16 +0300

    add_win

---
+++ script_load_win.sh
@@ -0,0 +1,2 @@
+git pull
+cp aboba_base.zip cp C:/Users/maksg/AppData/Roaming/Factorio/saves/
---
+++ script_save_win.sh
@@ -0,0 +1,3 @@
+cp C:/Users/maksg/AppData/Roaming/Factorio/saves/aboba_base.zip D:/Factorio_save_script/
+git commit -m 'save'
+git push origin main
